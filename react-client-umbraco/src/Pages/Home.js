24dd1c1132459a9914d4f4532662e2e8cdfd971c
unknown <iperish.x@gmail.com>
2019-10-24 11:43:25 +0200

    Created default routingg configuration and base router

---
+++ react-client-umbraco/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function HomePage() {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <p>
+        Omg its the home page of your project for Group 3.. er I mean Group 4!
+      </p>
+    </div>
+  )
+}
