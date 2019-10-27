bfa0446bb63d5babed2cb10ec0e74b8f371ff381
erlama <anaoleo0@gmail.com>
2019-10-27 16:31:29 +0100

    installed react-bootstrap

--- react-client-umbraco/src/App.js
+++ react-client-umbraco/src/App.js
@@ -4,18 +4,20 @@ import {Home} from './Pages/Home';
 import {Employee} from './Pages/Empoyee';
 import {Country} from './Pages/Country';
 import {Company} from './Pages/Company';
-//import Navbar from './Components/NavBar';
+import Navbar from './Components/NavBar';
 
 class App extends Component {
   render() {
      return (
-      <Switch>
-        {/* <Navbar/> */}
-        <Route exact path="/" component={Home} />
-        <Route path="/employee" component={Employee} />
-        <Route path="/country" component={Country} />
-        <Route path="/company" component={Company} />
-      </Switch>
+      <Navbar>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/employee" component={Employee} />
+          <Route path="/country" component={Country} />
+          <Route path="/company" component={Company} />
+        </Switch>
+      </Navbar>
+      
   )
   }
  
