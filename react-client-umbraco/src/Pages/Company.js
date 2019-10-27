5af8914f9205fb96638cb4d1c98ca9128461fde5
erlama <anaoleo0@gmail.com>
2019-10-28 00:51:15 +0100

    Bigger search bar

--- react-client-umbraco/src/Pages/Company.js
+++ react-client-umbraco/src/Pages/Company.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+let style = {
+  width: '500px'
+}
+
 export const Company = () => 
    (
     <div className="container">
       <h4>Companys</h4>
         <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <input style={style} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
