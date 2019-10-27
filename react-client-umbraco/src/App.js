4b686d548452de5bcd98b01fae7a75851b777bd1
erlama <anaoleo0@gmail.com>
2019-10-27 23:00:18 +0100

    Navbar finished

--- react-client-umbraco/src/App.js
+++ react-client-umbraco/src/App.js
@@ -4,20 +4,25 @@ import {Home} from './Pages/Home';
 import {Employee} from './Pages/Empoyee';
 import {Country} from './Pages/Country';
 import {Company} from './Pages/Company';
-import Navbar from './Components/NavBar';
+import {NavBar} from './Components/NavBar';
+import {Layout} from './Components/Layout';
+import {Jumbotron} from './Components/Jumbotron';
 
 class App extends Component {
   render() {
      return (
-      <Navbar>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/employee" component={Employee} />
-          <Route path="/country" component={Country} />
-          <Route path="/company" component={Company} />
-        </Switch>
-      </Navbar>
-      
+      <React.Fragment>
+        <NavBar/>
+        <Jumbotron/>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/employee" component={Employee} />
+              <Route path="/country" component={Country} />
+              <Route path="/company" component={Company} />
+            </Switch>
+          </Layout>
+      </React.Fragment>
   )
   }
  
--- react-client-umbraco/src/Components/NavBar.js
+++ react-client-umbraco/src/Components/NavBar.js
@@ -1,18 +1,53 @@
-import React from '../../node_modules/react'
-import {NavLink} from '../../node_modules/react-router-dom'
+import React from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
 
-const NavBar = () => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Contact book</a>
-    <div>
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item active"><NavLink className="nav-link" activeClassName="activate" to="/">Home</NavLink></li>
-            <li className="nav-item active"><NavLink className="nav-link" to="employee">Employee</NavLink></li>
-            <li className="nav-item active"><NavLink className="nav-link" to="country">Country</NavLink></li>
-            <li className="nav-item active"><NavLink className="nav-link" to="company">Company</NavLink ></li>
-        </ul>
-    </div>
-</nav>
+const Styles = styled.div`
+    .navbar {
+        background-color: #2c3333;
+    }
+
+    a, nav-brand, navbar-nav .nav-link{
+        color: #bbb;
+
+        &:hover {
+            color: white;
+        }
+    }
+`;
+
+export const NavBar = () => (
+    <Styles>
+        <Navbar expand="lg">
+            {/* <Nav.Link><NavLink to="/">Contakt book</NavLink></Nav.Link> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Item>
+                        <Nav.Link>
+                            <NavLink to="/">Home</NavLink>
+                        </Nav.Link>
+                    </Nav.Item>        
+                    <Nav.Item>
+                        <Nav.Link>
+                            <NavLink to="/employee">Employee</NavLink>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                            <NavLink to="/country">Country</NavLink>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link>
+                            <NavLink to="/company">Company</NavLink>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Styles>
 )
-       
-export default NavBar;
+
+
