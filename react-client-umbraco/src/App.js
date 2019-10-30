12e1d8006766e1c3f6ead929063f565d42192c70
erlama <anaoleo0@gmail.com>
2019-10-30 01:35:07 +0100

    Getting token from umbraco site

--- react-client-umbraco/src/App.js
+++ react-client-umbraco/src/App.js
@@ -7,8 +7,23 @@ import {Company} from './Pages/Company';
 import {NavBar} from './Components/NavBar';
 import {Layout} from './Components/Layout';
 import {Jumbotron} from './Components/Jumbotron';
+import { getAccessToke } from './Umbraco-API';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      content: [],
+      sponsors: []
+    }
+  }
+
+  componentDidMount(){
+    getAccessToke('w.tamim@hotmail.com', '1234567890')
+    .then((token) => {
+      this.getContent(token);
+    });
+  }
   render() {
      return (
       <React.Fragment>
@@ -16,7 +31,7 @@ class App extends Component {
         <Jumbotron/>
           <Layout>
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route data={this.state.content} exact path="/" component={Home} />
               <Route path="/employee" component={Employee} />
               <Route path="/country" component={Country} />
               <Route path="/company" component={Company} />
@@ -24,8 +39,28 @@ class App extends Component {
           </Layout>
       </React.Fragment>
   )
+  };
+
+  getContent(token){
+    fetch('http://localhost:65022', {
+      method: 'get',
+      headers: {
+        'Authorization': 'Bearer' + token,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      debugger
+      this.setState({
+        content: data._embedded.content['0'].properties,
+        sponsors: JSON.parse(data._embedded.content['0'].properties.sponsors),
+      });
+    });
   }
- 
 }
+
+
 
 export default App;
