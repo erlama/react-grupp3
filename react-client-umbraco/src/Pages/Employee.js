ff7f4629ad0312fff8ca4793133e2ea8bb438b49
Wade <w.tamim@hotmail.com>
2019-11-01 14:52:29 +0100

    Install table package and apply it on employees

--- react-client-umbraco/src/Pages/Employee.js
+++ react-client-umbraco/src/Pages/Employee.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getDataFromControllers } from "../getDataFromControllers";
+import { MDBDataTable } from "mdbreact";
 
 let style = {
   width: "500px"
@@ -9,7 +10,7 @@ let tableStyle = {
 };
 
 var _data;
-
+var tableData;
 export const Employee = () => {
   const [data, setData] = useState({});
 
@@ -23,48 +24,61 @@ export const Employee = () => {
   }, []);
 
   if (data._data !== undefined) {
-    data._data.forEach(element => {
-      returnList.push(
-        <tr key={element.Id}>
-          <th scope="row">{element.Id}</th>
-          <td>{element.FirstName}</td>
-          <td>{element.LastName}</td>
-          <td>{element.Department}</td>
-          <td>{element.Country}</td>
-        </tr>
-      );
-    });
+    // data._data.forEach(element => {
+    //   returnList.push(
+    //     <tr key={element.Id}>
+    //       <th scope="row">{element.Id}</th>
+    //       <td>{element.FirstName}</td>
+    //       <td>{element.LastName}</td>
+    //       <td>{element.Department}</td>
+    //       <td>{element.Country}</td>
+    //     </tr>
+    //   );
+    // });
+    tableData = {
+      columns: [
+        {
+          label: "Id",
+          field: "Id",
+          sort: "asc",
+          width: 150
+        },
+        {
+          label: "First name",
+          field: "FirstName",
+          sort: "asc",
+          width: 270
+        },
+        {
+          label: "Last name",
+          field: "LastName",
+          sort: "asc",
+          width: 200
+        },
+        {
+          label: "Department",
+          field: "Department",
+          sort: "asc",
+          width: 100
+        },
+        {
+          label: "Country",
+          field: "Country",
+          sort: "asc",
+          width: 150
+        }
+      ],
+      rows: data._data
+    };
+    console.log(data._data);
   } else {
     return <span>Loading ...</span>;
   }
   return (
     <div className="container">
       <h4>Employees</h4>
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          style={style}
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
       <div>
-        <table id="EmployeeTable" className="table" style={tableStyle}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Department</th>
-              <th>Country</th>
-            </tr>
-          </thead>
-          <tbody>{returnList}</tbody>
-        </table>
+        <MDBDataTable bordered hover data={tableData} />
       </div>
     </div>
   );
