eb2dad4b9cb990566ceedc5506b486deda197260
erlama <anaoleo0@gmail.com>
2019-11-01 16:07:34 +0100

    Country tableData

--- react-client-umbraco/src/Pages/Country.js
+++ react-client-umbraco/src/Pages/Country.js
@@ -1,49 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { getDataFromUmbraco } from "../getDataFromUmbraco";
+import {getDataFromControllers} from '../getDataFromControllers';
+import { MDBDataTable } from "mdbreact";
 
-let style = {
-  width: "500px"
-};
-
+let styles = {
+  bacground: 'blue'
+}
 var _data;
+var tableData;
 
 export const Country = () => {
   const [data, setData] = useState({});
 
-  const returnList = [];
   useEffect(() => {
-    var promiseResult = getDataFromUmbraco("1066", true);
+    var promiseResult = getDataFromControllers("countries/getAllCountries");
     promiseResult.then(result => {
-      _data = result._embedded.content;
+      _data = result;
       setData({ _data });
     });
   }, []);
 
-  console.log(data._data);
   if (data._data !== undefined) {
-    data._data.forEach(element => {
-      console.log(element.properties.adress);
-      returnList.push(<h6 key={element.id}>{element.properties.landName}</h6>);
-    });
+    tableData = {
+      columns: [
+        {
+          label: "Name",
+          field: "Name",
+          sort: "asc",
+          width: 270
+        },
+      ],
+      rows: data._data
+    };
+    console.log(data._data);
   } else {
     return <span>Loading ...</span>;
   }
   return (
     <div className="container">
       <h4>Countries</h4>
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          style={style}
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
-      <div>{returnList}</div>
+      <MDBDataTable bordered hover data={tableData} />
     </div>
   );
 };
